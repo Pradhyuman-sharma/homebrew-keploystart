7906140eba502597064d28a8211ec690c82986b2
Pradhyuman-sharma <sharmapuru0642@gmail.com>
2022-09-13 23:37:23 +0530

    added formula

---
+++ keploy.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Keploy < Formula
+  desc "No code API testing platform. Create unit tests and data mocks from API calls. "
+  homepage "https://keploy.io"
+  url "https://github.com/Pradhyuman-sharma/keploy/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "9593b463a5c6578626c6765e6a442c303b67c4cfe21db6a0e787d9bdf11b0ef7"
+  license "Apache-2.0"
+
+
+
+  def install
+        bin.install "keploy"
+  end
+end
