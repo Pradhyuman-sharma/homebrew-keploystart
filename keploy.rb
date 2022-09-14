c71346fa7f2ecb1583412c862bee571e04c488db
Pradhyuman-sharma <sharmapuru0642@gmail.com>
2022-09-14 17:25:30 +0530

    adding formula

--- keploy.rb
+++ keploy.rb
@@ -11,7 +11,7 @@ class Keploy < Formula
   depends_on "go" => :build
 
   def instal
-    ENV["GOPATH"] = DIR["cmd/server/*"]
+    prefic.install DIR["cmd/server/*"]
     system "go", "build", "-o" , "keploy"   
     bin.install "keploy"
   end
